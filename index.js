786be61cacd48b9913175b5d13c5c8ca2d5dd832
Inga Kononova <inga.kononova@gmail.com>
2024-04-18 19:08:19 +0300

    Added functionality for checking the number of clicks

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 let timerId; // переменная, которая будет хранить ID таймера
 
-const startButton = document.getElementById('start');
-startButton.addEventListener('click', function () {
-	timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+let firstClick = true; // создаём и присваиваем значение true. См. стр.8 и 19
+
+const startButton = document.getElementById("start");
+startButton.addEventListener("click", function () {
+  
+  if (firstClick) { // проверка, чтоб исключить повторный запуск: если true, значит ещё не запускалось (запуск ведёт за собой смену true на false, см.следующую стр.)
+    firstClick = false;
+    timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+  }
 });
 
-const stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', function () {
+firstClick = true;
+
+const stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", function () {
   clearInterval(timerId); // останавливаем таймер
+	firstClick = true; // присваеваем значение true, чтоб можно было опять запустить часы
 });
 
 function updateClock() {
-	const clock = document.getElementById('clock');
-	const now = new Date();
-	const hours = now.getHours();
-	const minutes = now.getMinutes();
-	const seconds = now.getSeconds();
-	clock.textContent = hours + ':' + minutes + ':' + seconds;
-};
+  const clock = document.getElementById("clock");
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  clock.textContent = hours + ":" + minutes + ":" + seconds;
+}
